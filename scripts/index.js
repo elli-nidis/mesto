77554b6953672ad747f08fc2b54d541037a8e0dd
ekaterina balabanidis <mythos.kath.nidis@gmail.com>
2023-02-15 23:41:15 +0300

    fix: исправить ошибки песле первого ревью

--- scripts/index.js
+++ scripts/index.js
@@ -1,54 +1,62 @@
-// нахожу кнопки редактирования профиля и закрытия попапа
+//нахожу кнопки редактирования профиля и закрытия попапа
 const profileEditButton = document.querySelector('.profile__edit-button');
 const popupCloseButton = document.querySelector('.popup__close-button');
-// нахожу попап
+
+//нахожу попап
 const userEditPopup = document.querySelector('.popup');
-// присваиваю значения юзеру
-const userName = 'Жак-Ив Кусто';
-const userOccupation = 'Исследователь океана';
-// нахожу поля для записи данных юзера в профиле и в попапе
+
+//нахожу поля для записи данных юзера в профиле и в попапе
 const profileUserName = document.querySelector('.profile__user-name');
 const profileUserOccupation = document.querySelector('.profile__user-occupation');
-const popupUserName = document.querySelector('.popup__user-name');
-const popupUserOccupation = document.querySelector('.popup__user-occupation');
-// нахожу форму
+const popupUserName = document.querySelector('.popup__input_user_name');
+const popupUserOccupation = document.querySelector('.popup__input_user_occupation');
+
+//нахожу форму
 const popupForm = document.querySelector('.popup__form');
 
-// заполняю значения юзера в профиле
-profileUserName.textContent = userName;
-profileUserOccupation.textContent = userOccupation;
 
-// заполняю значения юзера в попапе - тяну из профиля
-popupUserName.value = profileUserName.textContent;
-popupUserOccupation.value = profileUserOccupation.textContent;
-
-// добавляю слушателей на кнопки редактирования профиля и закрытия попапа
-profileEditButton.addEventListener('click', openUserEditPopup);
-popupCloseButton.addEventListener('click', closeUserEditPopup);
-
-popupForm.addEventListener('submit', handlePopupFormSubmit);
-
-// функция открытия попапа
+/**
+ * функция открытия попапа
+ */
 function openUserEditPopup() {
   userEditPopup.classList.add('popup_opened');
+
+//заполняю значения юзера в попапе - тяну из профиля
+  popupUserName.value = profileUserName.textContent;
+  popupUserOccupation.value = profileUserOccupation.textContent;
 }
 
-// функция закрытия попапа
+/**
+ * функция закрытия попапа
+ */
 function closeUserEditPopup() {
   userEditPopup.classList.remove('popup_opened');
 }
 
-// функция редактирования профиля через попап
+/**
+ * функция редактирования профиля через попап
+ * записывает в профиль новые значения пользователя, введённые в форму редактирования (папап)
+ */
 function handlePopupFormSubmit (event) {
-  // отменяю стандартную отправку формы
+  //отменяю стандартную отправку формы
   event.preventDefault();
-  // присваиваю новые значения в профиле, тяну из попапа
+
+//присваиваю новые значения в профиле, тяну из попапа
   profileUserName.textContent = popupUserName.value;
   profileUserOccupation.textContent = popupUserOccupation.value;
-  // закрываю попап
+
+//закрываю попап
   closeUserEditPopup();
 }
 
+//добавляю слушателя на кнопку редактирования профиля (открывает попап)
+profileEditButton.addEventListener('click', openUserEditPopup);
+
+//добавляю слушателя на кнопку закрытия попапа
+popupCloseButton.addEventListener('click', closeUserEditPopup);
+
+//Добавляю слушателя на кнопку Сохранить в попапе
+popupForm.addEventListener('submit', handlePopupFormSubmit);
 
 
 
