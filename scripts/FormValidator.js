821877c91f8efa38972e2d18f7092d9d3f91ec5e
ekaterina balabanidis <mythos.kath.nidis@gmail.com>
2023-04-05 23:40:44 +0300

    refactor: применить инкапсуляцию к классу FormValidator

--- scripts/FormValidator.js
+++ scripts/FormValidator.js
@@ -1,158 +1,136 @@
 class FormValidator {
   constructor(data, form) {
-    this.data = data;
-    // this.formSelector = data.formSelector;
-    this.inputSelector = data.inputSelector;
-    this.submitButtonSelector = data.submitButtonSelector;
-    this.inactiveButtonClass = data.inactiveButtonClass;
-    this.inputErrorClass = data.inputErrorClass;
-    this.errorClass = data.errorClass;
-    // this.popupError = data.popupError;
-    this.form = document.querySelector(form);
+    this._data = data;
+    this._inputSelector = data.inputSelector;
+    this._submitButtonSelector = data.submitButtonSelector;
+    this._inactiveButtonClass = data.inactiveButtonClass;
+    this._inputErrorClass = data.inputErrorClass;
+    this._errorClass = data.errorClass;
+    this._form = document.querySelector(form);
   }
 
-  //метод проверки наличия невалидного поля в форме
-  hasInvalidInput() {
-    console.log('метод hasInvalidInput работает');
-    return this.inputList.some(input => {
-      console.log('!input.validity.valid ' + !input.validity.valid);
+  //метод hasInvalidInput проверяет наличие невалидного поля в форме
+  _hasInvalidInput() {
+    return this._inputList.some(input => {
       //проверяю наличие хотя бы одного невалидного поля в массиве инпутов одной формы
       return !input.validity.valid;
     });
   };
 
-    //метод деактивации кнопки сабмит
+  //метод disableButtonState деактивирует кнопку сабмит
   disableButtonState() {
-    console.log('метод disableButtonState работает');
     //добавляю класс модификатора, устанавливаю деактивирующий кнопку атрибут
-    this.buttonElement.classList.add(this.inactiveButtonClass);
-    this.buttonElement.setAttribute('disabled', true);
+    this._buttonElement.classList.add(this._inactiveButtonClass);
+    this._buttonElement.setAttribute('disabled', true);
   };
 
-  //метод активации кнопки сабмит
-  activateButtonState() {
-    console.log('метод activateButtonState работает');
+  //метод activateButtonState активирует кнопку сабмит
+  _activateButtonState() {
     //удаляю класс модификатора, удаляю атрибут, деактивирующий кнопку
-    this.buttonElement.classList.remove(this.inactiveButtonClass);
-    this.buttonElement.removeAttribute('disabled');
+    this._buttonElement.classList.remove(this._inactiveButtonClass);
+    this._buttonElement.removeAttribute('disabled');
   }
 
-  //метод переключения состояния доступности кнопки сабмит в зависимости от наличия невалидных полей в форме
-  toggleButtonState() {
-    console.log('метод toggleButtonState работает');
-    if(this.hasInvalidInput()) {
+  //метод _toggleButtonState переключает состояние доступности кнопки сабмит в зависимости от наличия невалидных полей в форме
+  _toggleButtonState() {
+    if(this._hasInvalidInput()) {
       //если в форме есть невалидное поле,
       // делаю кнопку неактивной - вызываю для этого функцию disableButtonState
       this.disableButtonState();
     }
     else {
       //иначе делаю кнопку активной - вызываю для этого функцию activateBUttonState
-      this.activateButtonState();
+      this._activateButtonState();
     }
   };
 
-  //метод поиска места (тега), в котором выводится ошибка
-  findErrorPlace(input) {
-    console.log('работает метод findErrorPlace');
+  //метод _findErrorPlace ищет место (тега), в котором выводится ошибка
+  _findErrorPlace(input) {
     //нахожу атрибут name у инпута
-    this.inputName = input.getAttribute('name');
-  
+    this._inputName = input.getAttribute('name');
+
     //нахожу тег, в котором будет показана ошибка. Класс искомого тега содержит в себе [name] валидируемого поля
-    this.errorPlace = document.querySelector(`.${this.inputName}-error`);
-  
+    this._errorPlace = document.querySelector(`.${this._inputName}-error`);
+
     // возвращаю тег, в котором будет выведен текст ошибки
-    return this.errorPlace;
+    return this._errorPlace;
   };
 
-
-  //метод убирает слили для невалидного поля
-  removeStyleErrorInput(input) {
-    console.log('работает метод removeStyleErrorInput');
-    input.classList.remove(this.inputErrorClass);
+  //метод _removeStyleErrorInput убирает стили для невалидного поля
+  _removeStyleErrorInput(input) {
+    input.classList.remove(this._inputErrorClass);
   };
 
-  //метод скрывает текст ошибки валидации поля
-  hideInputError() {
-    console.log('работает метод hideInputError');
+  //метод _hideInputError скрывает текст ошибки валидации поля
+  _hideInputError() {
     //удаляю контент, удаляю класс
-    this.errorPlace.textContent = '';
-    this.errorPlace.classList.remove(this.errorClass);
+    this._errorPlace.textContent = '';
+    this._errorPlace.classList.remove(this._errorClass);
   };
 
-
-  //метод устанавливает слили для невалидного поля
-  setStyleErrorInput(input) {
-    console.log('работает метод setStyleErrorInput');
-    input.classList.add(this.inputErrorClass);
+  //метод _setStyleErrorInput устанавливает слили для невалидного поля
+  _setStyleErrorInput(input) {
+    input.classList.add(this._inputErrorClass);
   };
 
-  //метод показывает текст ошибки валидации поля
-  showInputError(input) {
-    console.log('работает метод showInputError');
+  //метод _showInputError показывает текст ошибки валидации поля
+  _showInputError(input) {
     //устанавливаю текст ошибки, добавляю класс, который делает тег с текстом ошибки видимым
-    this.errorPlace.textContent = input.validationMessage;
-    this.errorPlace.classList.add(this.errorClass);
+    this._errorPlace.textContent = input.validationMessage;
+    this._errorPlace.classList.add(this._errorClass);
   };
-  
 
-  //метод проверки поля на валидность
-  checkInputValidity(input) {
-    console.log('я внутри checkInputValidity, мой инпут ' + input);
+  //метод _checkInputValidity проверяет поля на валидность
+  _checkInputValidity(input) {
     if(input.validity.valid) {
       //если поле валидно, скрываю ошибку:
-      //нахожу место вывода ошибки, которую нужно скрыть. Место ищет функция findErrorPlace и возвращает нужный тег
-      this.errorPlace = this.findErrorPlace(input);
-  
-      //вызываю функцию, изменяющую стиль инпута
-      this.removeStyleErrorInput(input);
-  
-       //вызываю функцию, которая скрывает ошибку
-       this.hideInputError();
+      //нахожу место вывода ошибки, которую нужно скрыть. Место ищет метод _findErrorPlace и возвращает нужный тег
+      this._errorPlace = this._findErrorPlace(input);
+
+      //вызываю метод, изменяющий стиль инпута
+      this._removeStyleErrorInput(input);
+
+       //вызываю метод, который скрывает ошибку
+       this._hideInputError(this._errorPlace);
     }
     else {
       //если поле не валидно, показываю ошибку:
-      //нахожу место вывода ошибки, которую нужно показать. Место ищет ф-ция findErrorPlace и возвращает нужный тег
-      this.errorPlace = this.findErrorPlace(input);
+      //нахожу место вывода ошибки, которую нужно показать. Место ищет метод _findErrorPlace и возвращает нужный тег
+      this._errorPlace = this._findErrorPlace(input);
   
-      //вызываю функцию, изменяющую стиль инпута
-      this.setStyleErrorInput(input);
+      //вызываю метод, изменяющий стиль инпута
+      this._setStyleErrorInput(input);
   
-      //вызываю функцию, которая показывает ошибку
-      this.showInputError(input);
+      //вызываю метод, который показывает ошибку
+      this._showInputError(input);
     }
   };
 
-//метод включения валидации
-enableValidation(form) {
-  console.log('метод валидации работает');
-  console.log(this.form);
+  //метод enableValidation включает валидацию
+  enableValidation(_form) {
     //отменяю стандартное поведение формы при сабмите
-    //this.form.addEventListener('submit', evt => evt.preventDefault());
+    this._form.addEventListener('submit', evt => evt.preventDefault());
 
     //нахожу все инпуты в текущей форме
-    this.inputList = Array.from(this.form.querySelectorAll(this.inputSelector));
-    console.log(this.inputList);
+    this._inputList = Array.from(this._form.querySelectorAll(this._inputSelector));
 
     //нахожу в текущей форме кнопку с функцией submit
-    this.buttonElement = this.form.querySelector(this.submitButtonSelector);
-    console.log(this.buttonElement);
+    this._buttonElement = this._form.querySelector(this._submitButtonSelector);
 
-    //вызываю функцию toggleButtonState, которая управляет состоянием кнопки сабмит
-    this.toggleButtonState();
+    //вызываю метод _toggleButtonState, который управляет состоянием кнопки сабмит
+    this._toggleButtonState();
 
     //прохожусь по инпутам, навешиваю слушателя ошибок по событию ввода символа
-    this.inputList.forEach(input => {
+    this._inputList.forEach(input => {
       input.addEventListener('input', evt => {
-        //вызываю функцию checkInputValidity, которая проверяет валидность поля
-        this.checkInputValidity(input);
+        //вызываю метод _checkInputValidity, который проверяет валидность поля
+        this._checkInputValidity(input);
 
-        //вызываю функцию toggleButtonState, которая управляет состоянием кнопки сабмит
-        this.toggleButtonState(input);
+        //вызываю метод _toggleButtonState, который управляет состоянием кнопки сабмит
+        this._toggleButtonState(input);
       })
     });
-  
 };
-
 }
 
 export {FormValidator};
